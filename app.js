9faf8fab5c7959328974be9d86dc353cf3635084
Adrian Lee <adrilee@rim.com>
2012-03-20 13:55:31 -0400

    Added widget config.xml

--- app.js
+++ app.js
@@ -5,10 +5,10 @@ app.configure(function(){
 	app.use(express.bodyParser())
 	app.use(express.static(__dirname + '/public'))
 	app.use(express.errorHandler({ dumpExceptions: true, showStack: true}))
-})
+});
 
 app.get('/', function(req, res){
 	res.sendfile(__dirname + '/public/index.html');
-})
+});
 
-app.listen(3000)
+app.listen(3000);
