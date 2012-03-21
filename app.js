c8fafff3c620d7609e9762fcddcce85d24c2cb40
Adrian Lee <adrilee@rim.com>
2012-03-21 17:13:13 -0400

    Created placeholder functions to automate functional test on simulator, implement downloadDependency

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
-var express = require('express'),
-	app = express.createServer();
+var http = require('http'),
+    express = require('express'),
+    app = express.createServer(),
+    job = require('./lib/job');
   
-app.configure(function(){
-	app.use(express.bodyParser())
-	app.use(express.static(__dirname + '/public'))
-	app.use(express.errorHandler({ dumpExceptions: true, showStack: true}))
+app.configure(function() {
+    app.use(express.bodyParser())
+    app.use(express.static(__dirname + '/workspace/public'))
+    app.use(express.errorHandler({ dumpExceptions: true, showStack: true}))
 });
 
-app.get('/', function(req, res){
-	res.sendfile(__dirname + '/public/index.html');
+app.get('/', function(req, res) {
+    // res.sendfile(__dirname + '/public/index.html');
+    res.send("GET: /run/:job<br>GET: /run");
+});
+
+app.get('/run', function(req, res) {
+    // generate index html file and inject the jasmine specs into <head>
+    res.sendfile(__dirname + '/workspace/public/index.html');
+});
+
+app.get('/run/:job', function(req, res) {
+    job.run(req.params.job);
+    res.send(req.params.job);
 });
 
 app.listen(3000);
--- public/spec/blackberry.js
+++ public/spec/blackberry.js
@@ -4,11 +4,35 @@ describe("blackberry", function() {
 	});
 });
 
-describe("blackberry extensions", function() {
-	it('app should exist', function () {
-		expect(blackberry.app).toBeDefined();
+
+describe("blackberry.app", function() {
+	xit('blackberry.app.event should exist', function () {
+		expect(blackberry.app.event).toBeDefined();
 	});
 
+	it('blackberry.app.author should exist', function () {
+		expect(blackberry.app.author).toBeDefined('Research In Motion Ltd.');
+	});
+
+	xit('blackberry.app.authorEmail should exist', function () {
+		expect(blackberry.app.authorEmail).toBeDefined();
+	});
+
+	xit('blackberry.app.authorURL should exist', function () {
+		expect(blackberry.app.authorURL).toBeDefined();
+	});
+
+	xit('blackberry.app.copyright should exist', function () {
+		expect(blackberry.app.copyright).toBeDefined();
+	});
+
+	xit('blackberry.app.description should exist', function () {
+		expect(blackberry.app.description).toBeDefined();
+	});
+
+});
+
+describe("blackberry.invoke", function() {
 	it('invoke should exist', function () {
 		expect(blackberry.invoke).not.toBeDefined();
 	});
